02db48987def64214bb9d25fe61893fa3c23dad2
Łukasz Sobola <ukasz Sobola>
2017-05-10 23:01:48 +0200

    Last fixes for Task1

--- Task1/src/js/app.js
+++ Task1/src/js/app.js
@@ -4,46 +4,7 @@ import * as utils from './modules/utils';
 import * as customCheckbox from './modules/customCheckbox';
 import * as customSelect from './modules/customSelect';
 
-function ready(fn) {
-    if (document.readyState !== 'loading') {
-        fn();
-    } else {
-        document.addEventListener('DOMContentLoaded', fn);
-    }
-}
-
-//const forms = (function(window, document){
-//    
-//    const blockSubmit = function(element){
-//        const formsElems = element && document.querySelectorAll(element) ? document.querySelectorAll(element) : document.querySelectorAll('form');
-//            
-//        utils.forEach(formsElems, function(index, value){
-//            let thisForm = value;
-//            utils.addEvent(thisForm, 'submit', function(e){e.preventDefault();});
-//        });
-//    };
-//    
-//    return {
-//        block: blockSubmit
-//    };
-//}(window, document));
-
-function init() {
-    customCheckbox.init(); 
-//    customSelect.config({
-//        'customSelectButtonClass': 'dupa'
-//    });
+(function(){
+	customCheckbox.init();
     customSelect.init();
-//    forms.block();
-    
-//    (function blockSubmit(element){
-//        const formsElems = element && document.querySelectorAll(element) ? document.querySelectorAll(element) : document.querySelectorAll('form');
-//            
-//        utils.forEach(formsElems, function(index, value){
-//            let thisForm = value;
-//            utils.addEvent(thisForm, 'submit', function(e){e.preventDefault();});
-//        });
-//    }());
-}
-
-ready(init);
+}())
--- Task1/src/js/modules/customCheckbox.js
+++ Task1/src/js/modules/customCheckbox.js
@@ -25,7 +25,6 @@ function handleKeys(e){
             e.target.checked = true;
         }
     }
-    e.preventDefault();
 }
 
 function initCheckboxes(element){
