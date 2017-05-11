4f6363666c5b7acac879df746c0e2e072c82fe02
wooqash <l.sobola@hypermediaisobar.pl>
2017-05-11 15:24:41 +0200

    Added README file and removed unused files and folders

--- Task1/config/rollup/umd.js
+++
@@ -1,9 +0,0 @@
-import config from './es6.js';
-
-const packageInfo = require( '../../package.json' );
-
-config.format = 'umd',
-config.moduleName = 'bemquery';
-config.dest = `dist/${packageInfo.name}.umd.js`;
-
-export default config;
