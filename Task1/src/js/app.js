f6e4a17cec723667dfef359af0821ec1a8559c12
wooqash <l.sobola@hypermediaisobar.pl>
2017-06-05 15:26:31 +0200

    Checking Task1

--- Task1/src/js/app.js
+++ Task1/src/js/app.js
@@ -1,10 +1,9 @@
 'use strict';
 
-import * as utils from './modules/utils';
 import * as customCheckbox from './modules/customCheckbox';
 import * as customSelect from './modules/customSelect';
 
 (function(){
 	customCheckbox.init();
     customSelect.init();
-}())
+}());
